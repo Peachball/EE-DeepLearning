512977dfbbcf7b25bd54175bfd8321ed9e5fed38
Peachball <chenyexu@gmail.com>
2016-03-29 18:14:10 -0700

    Added handwriting data
    Former-commit-id: 85655aa6b8c491448cc6af31db82ba8199596413

--- DeepLearning.py
+++ DeepLearning.py
@@ -3,8 +3,79 @@ import theano.tensor as T
 import theano
 import numpy as np
 
+def readMNISTData(length=10000):
+    images = open('../train-images.idx3-ubyte', 'rb')
+    labels = open('../train-labels.idx1-ubyte', 'rb')
+    images.read(8)
+    labels.read(8)
+    def readInt(isn=True):
+        if isn:
+            return struct.unpack('>i', images.read(4))[0]
+        else:
+            return int.from_bytes(labels.read(4), byteorder='big', signed=True)
+    xsize = readInt()
+    ysize = readInt()
+    def readImage():
+        img = []
+        for i in range(xsize):
+            for j in range(ysize):
+                img.append(struct.unpack('>B', images.read(1))[0])
+        return img
+
+    def readLabel():
+        testLabel = [0]*10
+        testLabel[struct.unpack('B', labels.read(1))[0]] = 1
+        return testLabel
+
+    imgs = []
+    lbls = []
+    for i in range(length):
+        imgs.append(readImage())
+        lbls.append(readLabel())
+        print('\r Read {}/{}'.format(i+1, length), end="")
+    print('Done reading')
+    return (np.array(imgs), np.array(lbls))
+    
+def readcv(length=10000):
+    images = open('../t10k-images.idx3-ubyte', 'rb')
+    labels = open('../t10k-labels.idx1-ubyte', 'rb')
+    images.read(8)
+    labels.read(8)
+    def readInt(isn=True):
+        if isn:
+            return struct.unpack('>i', images.read(4))[0]
+        else:
+            return int.from_bytes(labels.read(4), byteorder='big', signed=True)
+    xsize = readInt()
+    ysize = readInt()
+    def readImage():
+        img = []
+        for i in range(xsize):
+            for j in range(ysize):
+                img.append(struct.unpack('>B', images.read(1))[0])
+        return img
+
+    def readLabel():
+        testLabel = [0]*10
+        testLabel[struct.unpack('B', labels.read(1))[0]] = 1
+        return testLabel
+
+    imgs = []
+    lbls = []
+    for i in range(length):
+        imgs.append(readImage())
+        lbls.append(readLabel())
+        print('\r Read {}/{}'.format(i+1, length), end="")
+        
+    print ('Done Reading')
+    return (np.array(imgs), np.array(lbls))
+
+
 class Layer:
-    def __init__(self, in_size, out_size, no_compile=True, layer_type='sigmoid', in_var=None):
+    def __init__(self, in_size, out_size, no_compile=True, layer_type='sigmoid', in_var=None,
+            init_size=0.1):
+        self.in_size = in_size
+        self.out_size = out_size
         if not layer_type in ['sigmoid', 'tanh', 'lstm', 'rnn']:
             raise Exception('Layer type is invalid: ' + str(layer_type))
         
@@ -12,9 +83,18 @@ class Layer:
             x = T.matrix('Input')
         else:
             x = in_var
-
+        
         if layer_type in ['sigmoid', 'tanh']:
-            pass
+            self.w = theano.shared((np.random.rand(in_size, out_size) - 0.5) *
+                    init_size).astype(theano.config.floatX)
+            self.b = theano.shared((np.random.rand(out_size) - 0.5) *
+                    init_size).astype(theano.config.floatX)
+            if layer_type == 'sigmoid':
+                self.out = T.nnet.sigmoid( x * self.w + self.b )
+            if layer_type == 'tanh':
+                self.out = T.tanh( x * self.w + self.b )
+            
+            self.params = [self.w, self.b]
 
 
     def predict(self, x):
@@ -25,3 +105,9 @@ class Layer:
 
     def getOutput(self):
         pass
+
+class AutoEncoder:
+    def __init__(self, *dim, **kwargs):
+        momentum = kwargs.get('momentum', 0)
+        alpha = kwargs.get('alpha', 0.01)
+        rprop = kwargs.get('rprop', False)
