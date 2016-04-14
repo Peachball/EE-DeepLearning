6c703cc6d3080beb1e6704483c5b8de681395252
Peachball <chenyexu@gmail.com>
2016-04-13 21:03:34 -0700

    Updates work although they are shady as hell
    Former-commit-id: 41189f73bd73bf3ee110be96f29adf44f4ad39a3

--- RBM.py
+++ RBM.py
@@ -7,7 +7,8 @@ from DeepLearning import readMNISTData, readcv
 
 class RBMLayer:
     def __init__(self, in_size, out_size, visible_type='det', hidden_type='det',
-            distribution=T.nnet.sigmoid, init_size=0.1, in_var=T.matrix('input')):
+            distribution=T.nnet.sigmoid, init_size=0.1, in_var=T.matrix('input'),
+            persistent_updatesize=100):
         self.in_size = in_size
         self.out_size = out_size
         self.distribution = distribution
@@ -25,21 +26,28 @@ class RBMLayer:
         self.out = distribution(T.dot(hidden, self.w.T) + self.b)
         self._getSample = theano.function([in_var], self.out)
 
-    def CDUpdates(self, x, alpha, negativeSample):
+        self.persistentCD = theano.shared(value=(np.random.rand(persistent_updatesize,
+            in_size)-0.5)*init_size, name='Persistent CD')
 
+    def CDUpdates(self, x, alpha):
+
+        negativeSample = self.persistentCD
         updates = []
 
         #Weight updates
-        hidden = self.distribution(T.dot(x, self.w) + self.b)
-        negHidden = self.distribution(T.dot(negativeSample, self.w) + self.b)
-        print(hidden.shape, negHidden.shape)
-#        weightGrad = T.dot(x, hidden) / x.shape[0] - T.dot(negativeSample, negHidden) / \
-#            negativeSample.shape[0]
-#        updates.append((self.w, self.w + alpha * (weightGrad)))
+        hidden = self.distribution(T.dot(x, self.w) + self.c)
+        negHidden = self.distribution(T.dot(negativeSample, self.w) + self.c)
+        weightGrad = (T.dot(x.T, hidden))/x.shape[0] - T.dot(negativeSample.T, negHidden)/negHidden.shape[0]
+        updates.append((self.w, self.w + alpha * (weightGrad)))
 
         #Bias updates
-#        updates.append((self.b, self.b + T.mean(alpha * (x - negativeSample))))
-        updates.append((self.c, self.c + (alpha * (hidden - negHidden))))
+        updates.append((self.b, self.b + T.mean(alpha * (x - negativeSample), axis=0)))
+        updates.append((self.c, (self.c + T.mean(alpha * (hidden - negHidden), axis=0))))
+
+        #Persistent Contrastive Divergence
+        newCD_h = self.distribution(T.dot(self.persistentCD, self.w) + self.c)
+        newCD_v = self.distribution(T.dot(newCD_h, self.w.T) + self.b)
+        updates.append((self.persistentCD, newCD_v))
         return updates
 
     def gibbSample(self, startSample, k=1):
@@ -47,13 +55,23 @@ class RBMLayer:
         for i in range(k):
             sample = self._getSample(sample)
         return sample
+    
+    def miniBatch(self, learnFunction, x, epochs=1, verbose=False):
+        train_error = []
+        batchSize = self.persistentCD.get_value().shape[0]
+
+        for i in range(epochs):
+            for j in range(0, x.shape[0] - batchSize, batchSize):
+                learnx = x[j:(j+batchSize)]
+                error = learnFunction(learnx, learnx)
+                if verbose: print(error, "Epoch:", i + j/x.shape[0])
+                train_error.append(error)
 
 def RBMTester():
-    images, labels = readMNISTData(10)
+    images, labels = readMNISTData(1000)
 
     images = images / images.max()
 
-    print(images.shape)
 
     rbm = RBMLayer(784, 600)
 
@@ -61,18 +79,17 @@ def RBMTester():
     mse = T.mean(T.sqr(rbm.out - y))
 
     negSample = theano.shared(value=rbm.gibbSample(images)).astype(theano.config.floatX)
-    print(rbm.c.get_value().shape)
-    updates = rbm.CDUpdates(rbm.in_var, 0.01, negSample)
+    updates = rbm.CDUpdates(rbm.in_var, 0.01)
 
-    learn = theano.function([rbm.in_var, y], mse, updates=updates, mode='DebugMode')
 
-    print(learn(images, images))
-    
+    learn = theano.function([rbm.in_var, y], mse, updates=updates)
+
+    rbm.miniBatch(learn, images, verbose=True, epochs=100)
     sampled = rbm.gibbSample(images)
 
-    print("ok lol")
-    plt.imshow(sampled[0].reshape(28, 28), cmap='Greys', interpolation='none')
-    plt.show()
+    for i in range(10):
+        plt.imshow(sampled[i].reshape(28, 28), cmap='Greys', interpolation='none')
+        plt.show()
     
 
 if __name__== '__main__':
