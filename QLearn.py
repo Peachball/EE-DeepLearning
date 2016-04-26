c2bad52ae04fafa1629ae77d8130fefdc3f64546
Peachball <chenyexu@gmail.com>
2016-04-25 19:37:32 -0700

    Works for a 5x5 but not for other sizes yet
    Former-commit-id: 79dde1238263bf3c8f02205c49935db75fcb9e15

--- QLearn.py
+++ QLearn.py
@@ -8,6 +8,8 @@ from DeepLearning import Layer
 from DeepLearning import FFClassifier
 from DeepLearning import generateVanillaUpdates
 from DeepLearning import generateRpropUpdates
+from DeepLearning import generateMomentumUpdates
+from DeepLearning import generateRmsProp
 import theano
 import theano.tensor as T
 
@@ -53,30 +55,35 @@ def nextIter(board, direction):
     
     if board[head[0]][head[1]] < 0:
         board[head[0]][head[1]] = size + 1
-        return size
+        return size+1
     
     if board[head[0]][head[1]] > 0:
-        return -size
+        board[head[0]][head[1]] = size
+        return -(size)
 
     board[head[0]][head[1]] = size
     return 0
 
 def addApple(board, random=True):
     if random:
-        rng = np.random.randint(len(board) * len(board[0]) - evalSize(board)[0])
+        rng = np.random.randint(2 * len(board) * len(board[0]) - evalSize(board)[0])
     else:
         rng = 1000
     
     spaces = 0
-    for i in range(len(board)):
+    i = -1
+    while spaces <= rng:
+        i += 1
         for j in range(len(board)):
-            if spaces >= rng and board[i][j] == 0:
+            if board[i][j] > 0:
+                continue
+            if spaces == rng:
                 board[i][j] = -1
-                return
+                return 0
             spaces += 1
         if i == len(board) - 1:
             i = 0
-    print("Add apple has a problem")
+    return 1
 
 def clear(win):
     win.fill((255, 255, 255))
@@ -90,6 +97,7 @@ def createBoard(size):
     return board
 
 def display(board, win):
+    [_, (headx, heady)] = evalSize(board)
     def draw(pos, state):
         sizex =  width / len(board)
         sizey = height / len(board[0])
@@ -100,6 +108,8 @@ def display(board, win):
             color = (255, 0, 0)
         elif state=='snake':
             color = (0, 0, 0)
+        elif state=='head':
+            color = (0, 255, 0)
         else:
             color = (255, 255, 255)
         pygame.draw.rect(win, color, ((sizex*(pos[0])),(sizex*(pos[1])),sizex, sizey))
@@ -111,6 +121,8 @@ def display(board, win):
                 state='snake'
             if board[i][j] < 0:
                 state='apple'
+            if i == headx and j == heady:
+                state='head'
             draw((i, j), state)
 
 def convertBoard(board, style='legit'):
@@ -124,12 +136,12 @@ def convertBoard(board, style='legit'):
                 if xpo < 0:
                     xpo += len(board)
                 if ypo < 0:
-                    ypo += len(board)
+                    ypo += len(board[0])
                 
                 if board[i][j] > 0:
                     output[2 * (xpo * len(board[0]) + ypo)] = board[i][j]
                 if board[i][j] < 0:
-                    output[2 * (xpo * len(board[0]) + ypo)] = 1
+                    output[2 * (xpo * len(board[0]) + ypo) + 1] = 1
         return output
     
     if style=='shady':
@@ -154,12 +166,18 @@ def convertBoard(board, style='legit'):
                     output.append(0)
         return output
 
-def predict(prediction, temperature=1):
+
+def predict(prediction, temperature=1, noDir=-1):
     total = np.sum(np.power(np.e, prediction/temperature))
+    if noDir >= 0:
+        noDir = (noDir + 2) % 4
+        total -= np.power(np.e, prediction[0, noDir]/temperature)
     rng = np.random.rand()
     direction = 3
     b = 0
     for i in range(4):
+        if i == noDir:
+            continue
         if rng < b + pow(np.e, prediction[0,i]/temperature)/total:
             direction = i
             break
@@ -167,92 +185,115 @@ def predict(prediction, temperature=1):
     return direction
 
 def LearnSnake():
-    boardSize = 5
+    boardSize = 10
     scr = pygame.display.set_mode((width, height))
-    pygame.display.flip()
     b = createBoard(boardSize)
     b[0][1] = 1
     addApple(b)
-    q1 = FFClassifier(boardSize * boardSize * 3, 700, init_size=0.01)
-    q2 = Layer(700, 4, layer_type='rlu', init_size=0.01, in_var=q1.out)
+    q1 = FFClassifier(boardSize * boardSize * 2, 700, 300, init_size=0.01)
+    q2 = Layer(300, 4, layer_type='linear', init_size=0.01, in_var=q1.out)
+
+    x = T.matrix('input')
+    q3 = Layer(boardSize * boardSize * 2, 4, init_size=0.01, layer_type='linear', in_var=x)
 
     params = q1.params + q2.params
     out = q2.out
     y = T.matrix('output')
     mse = T.mean(T.sqr(out  - y))
 
-    updates = generateVanillaUpdates(params, 0.1, mse)
-    (storage, rupdates) = generateRpropUpdates(params, mse, init_size=0.1)
-    learn = theano.function([q1.x, y], mse, updates=updates)
+    updates = generateVanillaUpdates(params, 0.01, mse)
+    (storage, rupdates) = generateRpropUpdates(params, mse, init_size=0.01)
+    (storage, mupdates) = generateMomentumUpdates(params, 0.5, 0.01, mse)
+    ([_, _, alpha], rms) = generateRmsProp(params, 0.01, 0.9, mse)
+    learn = theano.function([q1.x, y], mse, updates=rms)
+    rlearn = theano.function([q1.x, y], mse, updates=mupdates)
     p = theano.function([q1.x], out)
-
-    nextIter(b, 0)
-
 
     print("time to watch some snake")
     #Snake Drawing things
     
-    lam = 1
+    lam = 0.9
     maxMoves = boardSize * boardSize // 2
     mem = []
     rew = []
     act = []
     nstate = []
     maxSize = 0
+    maxMem = 10000
     draw = False
-    style = 'shady'
+    style = 'legit'
+    maxMoves = -1
     if style == 'shady':
         inputsize = 3 * boardSize ** 2
     elif style=='legit':
         inputsize = 2 * boardSize**2
     temp = 1
-    maxMoves = 1000
     while True:
         moves = 0
         tooSlow = 0
+        prevMove = -1
         while True:
             moves += 1
             bef = np.array(convertBoard(b, style=style)).reshape(1, inputsize)
             prediction = p(bef)
-            move = predict(prediction, temperature=temp)
-
+            move = predict(prediction, temperature=temp, noDir=-1)
+            prevMove = move
             if draw:
                 clear(scr)
                 display(b, scr)
                 pygame.display.update()
+                time.sleep(0.1)
             status = nextIter(b, move)
             after = np.array(convertBoard(b, style=style)).reshape(1, inputsize)
     
             r = 0
             tooSlow += 1
             if status > 0:
-                addApple(b)
+                if addApple(b) == 1:
+                    print("AI Won!")
+                    break
                 r = 1
                 tooSlow = 0
             end = False
-            if status < 0 or tooSlow > maxMoves:
-                r = 0
+            if status < 0 or tooSlow > maxMoves > 0:
+                if status < 0:
+                    r = -1
                 end = True
-
-            #Training part
-            correct = prediction
-            correct[0,move] = r + lam * np.max(p(after))
-
-            if end:
-                correct = np.array([0]*4)
 
             mem.append(np.squeeze(bef))
             nstate.append(np.squeeze(after))
             act.append(move)
             rew.append(r)
-            print(p(bef))
+#            print(p(bef).max() - p(bef).min())
+#            print(p(bef))
             if end:
                 break
         [size, _] = evalSize(b)
         if size > maxSize:
             maxSize = size
             print("Max Size: ", size)
-        if len(mem) > 100:
+            print("Temperature: ", temp)
+
+            #Play a sample game of snake
+            status = 0
+            board = createBoard(boardSize)
+            board[0][0] = 1
+            addApple(board)
+            prevMove = -1
+            while status >= 0:
+                clear(scr)
+                display(board, scr)
+                pygame.display.flip()
+                time.sleep(0.1)
+                c = np.array(convertBoard(board, style=style)).reshape(1, inputsize)
+                move = predict(p(c), temperature=temp, noDir=prevMove)
+                prevMove = move
+                status = nextIter(board, move)
+                if status > 0:
+                    if addApple(board) == 1:
+                        break
+
+        if len(mem) > 1000:
             def pickSamples(size=100):
                 samples = random.sample(range(len(mem)), size)
                 
@@ -260,10 +301,10 @@ def LearnSnake():
                         [rew[i] for i in samples],
                         [act[i] for i in samples],
                         [nstate[i] for i in samples])
-            (m, r, a, s) = pickSamples()
             def getTarget(m, r, a, s):
                 cor = p(np.array(m))
-                cor[range(len(cor)),a] = lam * np.max(p(np.array(s)), axis=1) + r
+                cor[range(len(cor)),a] = np.greater_equal(r, 0) * (lam * np.max(p(np.array(s)),
+                    axis=1)) + r
                 '''
                 print(cor[range(len(cor)), a])
                 print("Prediction:", p(np.array(s)))
@@ -273,16 +314,31 @@ def LearnSnake():
                 raw_input('hm')
                 '''
                 return cor
-            cor = getTarget(m, r, a, s)
             
             def genData(size=100):
-                data = pickSamples()
-                return data[0], getTarget(data)
+                data = pickSamples(size)
+                return data[0], getTarget(data[0], data[1], data[2], data[3])
             error = 10
             for j in range(100):
-                error = (learn(np.array(m), np.array(cor)))
+                a, b = genData(size=32)
+#                a, b = mem, getTarget(mem, rew, act, nstate)
+                a = np.array(a)
+                b = np.array(b)
+                error = (learn(a, b))
 #                print(error)
-#            print(error)
+            '''
+            mem = []
+            rew = []
+            act = []
+            nstate = []
+            '''
+            if temp > 0.1:
+                temp *= 0.999
+        if len(mem) > maxMem:
+            del mem[:-maxMem]
+            del rew[:-maxMem]
+            del act[:-maxMem]
+            del nstate[:-maxMem]
         b = createBoard(boardSize)
         b[0][0] = 1
         addApple(b)
