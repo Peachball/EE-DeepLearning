00a130378c37303488fb27cd1bb00ada968fd75a
Peachball <chenyexu@gmail.com>
2016-05-04 15:55:09 -0700

    Restructured lstm so that it will use gpu
    Former-commit-id: b7eed0a6daa7fac679af3fe93fd87238deaa7e67

--- RecurrentNetworks.py
+++ RecurrentNetworks.py
@@ -34,7 +34,7 @@ class RecurrentLayer:
             size=(hidden_size, out_size)))
         b_ho = theano.shared(value=np.random.uniform(low=-init_size, high=init_size,
             size=(out_size)))
-        hidden = theano.shared(
+        self.hidden = theano.shared(
                 value=np.zeros(hidden_size)).astype(theano.config.floatX)
 
         def recurrence(x, h_tm1):
@@ -47,6 +47,7 @@ class RecurrentLayer:
 
         self.out = out
         self.updates = updates
+        self.hidden = hidden[-1]
 
         self.params = [w_ih, b_ih, w_hh, b_hh, w_io, b_io, w_ho, b_ho]
 
@@ -88,54 +89,76 @@ class LSTMLayer:
             print('Constants have been initalized')
 
         #Forget gate
-        self.W_xf = theano.shared(value=init_size*np.random.rand(in_size, cell_size), name='x to \
-                forget gate').astype(config.floatX)
-        self.W_hf = theano.shared(value=init_size*np.random.rand(out_size, cell_size), name='h to \
-                forget gate').astype(config.floatX)
-        self.W_cf = theano.shared(value=init_size*np.random.rand(cell_size, cell_size), name='cell \
-                to forget gate').astype(config.floatX)
-        self.b_f = theano.shared(init_size*np.random.rand(1, cell_size), name='forget \
-                bias').astype(config.floatX)
+        self.W_xf = theano.shared(
+                value=np.random.uniform(low=-init_size, high=init_size,
+                    size=(in_size, cell_size)).astype(theano.config.floatX))
+        self.b_f= theano.shared(
+                value=np.random.uniform(low=-init_size, high=init_size,
+                    size=(1, cell_size)).astype(theano.config.floatX))
+        self.W_cf = theano.shared(
+                value=np.random.uniform(low=-init_size, high=init_size,
+                    size=(cell_size, cell_size)).astype(theano.config.floatX))
+        self.W_hf = theano.shared(
+                value=np.random.uniform(low=-init_size, high=init_size,
+                    size=(out_size, cell_size)).astype(theano.config.floatX))
 
 #        forget = T.nnet.sigmoid(T.dot(self.h, self.W_hf) + T.dot(self.C, self.W_cf) + T.dot(self.x, self.W_xf) + self.b_f)
 
         #Memories
-        self.W_hm = theano.shared(value=init_size*np.random.rand(out_size, cell_size), name='h to \
-                memories').astype(config.floatX)
-        self.W_xm = theano.shared(value=init_size*np.random.rand(in_size, cell_size), name='x to \
-                memories').astype(config.floatX)
-        self.b_m = theano.shared(init_size*np.random.rand(1, cell_size), name='memory \
-                bias').astype(config.floatX)
+        self.W_hm = theano.shared(
+                value=np.random.uniform(low=-init_size, high=init_size,
+                    size=(out_size, cell_size)).astype(theano.config.floatX))
+        self.W_xm = theano.shared(
+                value=np.random.uniform(low=-init_size, high=init_size,
+                    size=(in_size, cell_size)).astype(theano.config.floatX))
+        self.b_m = theano.shared(
+                value=np.random.uniform(low=-init_size, high=init_size,
+                    size=(1, cell_size)).astype(theano.config.floatX))
 
 #        memories = T.tanh(T.dot(self.h, self.W_hm) + T.dot(self.x, self.W_xm) + self.b_m)
-
         #Remember Gate
-        self.W_hr = theano.shared(value=init_size*np.random.rand(out_size, cell_size), name='h to \
-                remember').astype(config.floatX)
-        self.W_cr = theano.shared(value=init_size*np.random.rand(cell_size, cell_size), name='cell to \
-        remember').astype(config.floatX)
-        self.W_xr = theano.shared(value=init_size*np.random.rand(in_size, cell_size), name='x to \
-                remember').astype(config.floatX)
-        self.b_r = theano.shared(value=init_size*np.random.rand(1, cell_size), name='remember \
-                bias').astype(config.floatX)
+        self.W_hr = theano.shared(
+                value=np.random.uniform(low=-init_size, high=init_size,
+                    size=(out_size, cell_size)).astype(theano.config.floatX))
+        self.W_xr = theano.shared(
+                value=np.random.uniform(low=-init_size, high=init_size,
+                    size=(in_size, cell_size)).astype(theano.config.floatX))
+        self.W_cr = theano.shared(
+                value=np.random.uniform(low=-init_size, high=init_size,
+                    size=(cell_size, cell_size)).astype(theano.config.floatX))
+        self.b_r = theano.shared(
+                value=np.random.uniform(low=-init_size, high=init_size,
+                    size=(1, cell_size)).astype(theano.config.floatX))
 
 #        remember = T.nnet.sigmoid(T.dot(self.h, self.W_hr) + T.dot(self.C, self.W_cr) + T.dot(self.x, self.W_xr) + self.b_r)
 
         #Output
-        self.W_co = theano.shared(value=init_size*np.random.rand(cell_size, out_size),
-            name='cell to out').astype(config.floatX)
-        self.W_ho = theano.shared(value=init_size*np.random.rand(out_size, out_size),
-            name='hidden to out').astype(config.floatX)
-        self.W_xo = theano.shared(value=init_size*np.random.rand(in_size, out_size), name='x to out')
-        self.b_o = theano.shared(value=init_size*np.random.rand(1, out_size), name='out bias')
+        self.W_co = theano.shared(
+                value=np.random.uniform(low=-init_size, high=init_size,
+                    size=(cell_size, out_size)).astype(theano.config.floatX))
+        self.W_ho = theano.shared(
+                value=np.random.uniform(low=-init_size, high=init_size,
+                    size=(out_size, out_size)).astype(theano.config.floatX))
+        self.W_xo = theano.shared(
+                value=np.random.uniform(low=-init_size, high=init_size,
+                    size=(in_size, out_size)).astype(theano.config.floatX))
+        self.b_o = theano.shared(
+                value=np.random.uniform(low=-init_size, high=init_size,
+                    size=(1, out_size)).astype(theano.config.floatX))
 
         #Hidden
-        self.W_ch = theano.shared(value=init_size*np.random.rand(cell_size, out_size), name='cell to hidden')
-        self.W_hh = theano.shared(value=init_size*np.random.rand(out_size, out_size),
-            name='hidden to hidden')
-        self.W_xh =theano.shared(value=init_size*np.random.rand(in_size, out_size), name='x to hidden')
-        self.b_h =theano.shared(value=init_size*np.random.rand(1, out_size), name='bias for hidden')
-
+        self.W_ch = theano.shared(
+                value=np.random.uniform(low=-init_size, high=init_size,
+                    size=(cell_size, out_size)).astype(theano.config.floatX))
+        self.W_hh = theano.shared(
+                value=np.random.uniform(low=-init_size, high=init_size,
+                    size=(out_size, out_size)).astype(theano.config.floatX))
+        self.W_xh = theano.shared(
+                value=np.random.uniform(low=-init_size, high=init_size,
+                    size=(in_size, out_size)).astype(theano.config.floatX))
+        self.b_h = theano.shared(
+                value=np.random.uniform(low=-init_size, high=init_size,
+                    size=(1, out_size)).astype(theano.config.floatX))
 
         self.params = [self.W_xf, self.W_hf, self.W_cf, self.b_f, self.W_hm, self.W_xm, self.b_m,
                 self.W_hr, self.W_cr, self.W_xr, self.b_r, self.W_co, self.W_ho,
@@ -300,7 +323,8 @@ def RNNTest():
     predict = theano.function([x], out, updates=l1.updates+l2.updates)
     mse = T.mean(T.sqr(out - y))
 
-    (m, adadelta) = generateAdadelta(l1.params + l2.params, mse, alpha=1)
+    params = l1.params + l2.params
+    (m, adadelta) = generateAdadelta(params, mse, alpha=1)
 
     learn = theano.function([x, y], mse, updates=adadelta)
 
