db86d9189a027447f3d58ca6e9d270301f2b9ed2
Peachball <chenyexu@gmail.com>
2016-09-25 21:23:49 -0700

    Futher testing on why convrbms not working

--- RBM.py
+++ RBM.py
@@ -183,29 +183,26 @@ class ConvRBMLayer:
 
     def sample_vhv(self, v, pool=None):
         [h_act, h_sig, h_bin] = self.getHidden(v)
-        [v_act, v_sig, v_bin] = self.getVisible(h_bin)
+        [v_act, v_sig, v_bin] = self.getVisible(h_sig)
         return v_sig, v_bin
 
     def mean_vhv(self, v, pool=None):
-        v_tm1bin = self.theano_rng.binomial(size=v.shape, n=1, p=v,
-                dtype=theano.config.floatX)
-        [h_act, h_sig, h_bin] = self.getHidden(v_tm1bin)
-        [v_act, v_sig, v_bin] = self.getVisible(h_bin)
+        # v_tm1bin = self.theano_rng.binomial(size=v.shape, n=1, p=v,
+                # dtype=theano.config.floatX)
+        [h_act, h_sig, h_bin] = self.getHidden(v)
+        [v_act, v_sig, v_bin] = self.getVisible(h_sig)
         return v_sig
 
-    def getCDUpdates(self, v, persistent, steps=1, pooling=None, sparsity=False):
-        v_bin = self.theano_rng.binomial(size=v.shape, n=1, p=v,
-                dtype=theano.config.floatX)
-        visible_energy = self.free_energy(v_bin)
-
+    def getPCDUpdates(self, v, persistent, steps=1, pooling=None, sparsity=False):
         def gibbsSample(prev_sample):
             v_sig, v_bin = self.sample_vhv(prev_sample)
             return v_bin, v_sig
 
-        [samples, _], updates = theano.scan(fn=gibbsSample,
+        [samples, sig_samples], updates = theano.scan(fn=gibbsSample,
                 outputs_info=[persistent, None], n_steps=steps)
 
         new_chain = samples[-1]
+        sig_sample = sig_samples[-1]
         upd = list(updates.items()) + [(persistent, new_chain)]
         energy = T.mean(self.free_energy(v)) -\
                 T.mean(self.free_energy(new_chain))
@@ -215,6 +212,34 @@ class ConvRBMLayer:
 
         grad = T.grad(energy, self.params, consider_constant=[new_chain])
         return upd, energy, grad
+
+    def getCDUpdates(self, v, cd_size, steps=1, pooling=None):
+        def gibbsSample(prev_sample):
+            v_sig, v_bin = self.sample_vhv(prev_sample)
+            return v_bin, v_sig
+
+        [samples, sig_samples], updates = theano.scan(gibbsSample,
+                outputs_info=[v[:cd_size], None], n_steps=steps)
+
+        new_chain = samples[-1]
+        sig_sample = sig_samples[-1]
+        upd = list(updates.items())
+        free_energy = T.mean(self.free_energy(v)) -\
+                T.mean(self.free_energy(sig_sample))
+
+        grad = T.grad(free_energy, self.params, consider_constant=[sig_sample])
+        return upd, free_energy, grad
+
+def getMomentumUpdates(grad, params, alpha=0.1, momentum=0.9):
+    mparams = []
+    updates = []
+    for p, g in zip(params, grad):
+        m = theano.shared(np.zeros(p.get_value().shape)
+                .astype(theano.config.floatX))
+        mparams.append(m)
+        updates.append((p, p - m))
+        updates.append((m, momentum * m + alpha * g))
+    return updates
 
 def RBMTester():
     images, labels = readMNISTData(10000)
@@ -251,22 +276,26 @@ def RBMTester():
         plt.imshow(sampled[0].reshape(28, 28), cmap='Greys', interpolation='none')
         plt.show()
 
+def load_simple_image(filename):
+    from PIL import Image
+    image = Image.open(filename)
+    img = np.array(image).astype('float32') / 255
+    img = img.reshape(-1, 32, 32, 3)
+    img = img.transpose(0, 3, 1, 2)
+    return img
+
 def ConvRBMTester():
     from keras.datasets import cifar10
     (X_train, _), _ = cifar10.load_data()
     X_train = X_train.astype('float32') / 255
     X = T.tensor4()
-    x_mnist, _ = readMNISTData(10000)
-    x_mnist = x_mnist.reshape(-1, 1, 28, 28) / 255
-    print(x_mnist.shape)
-    rbm = ConvRBMLayer(3, 24, (9, 9))
-    _, _, hid_bin = rbm.getHidden(X)
+    rbm = ConvRBMLayer(3, 24, (3, 3))
     energy = rbm.free_energy(X)
     persistent = theano.shared(np.random.rand(2, 3, 32, 32).astype(theano.config.floatX))
-    upd, error, grad = rbm.getCDUpdates(X, persistent, steps=2, sparsity=False)
+    upd, error, grad = rbm.getCDUpdates(X, 2, steps=1)
 
     origupd = upd
-    upd += [(p, p - 0.3 * g) for p, g in zip(rbm.params, grad)]
+    upd += [(p, p - 0.1 * g) for p, g in zip(rbm.params, grad)]
 
     learn = theano.function([X], error, updates=upd, allow_input_downcast=True)
     generate = theano.function([X], rbm.mean_vhv(X),
@@ -341,5 +370,31 @@ def basic_ConvRBMTester():
     plt.imshow(x_mnist[1, 0] * 255, cmap='Greys')
     plt.show()
 
+def med_ConvRBMTester():
+    plt.ion()
+    img = load_simple_image('color.png')
+    plt.imshow(img[0].transpose(1, 2, 0))
+    rbm = ConvRBMLayer(3, 3, (3, 3))
+    print(rbm.params)
+    weights = np.zeros((3, 3, 3, 3), dtype='float32')
+    weights[:,:,1,1] = 1
+    rbm.params[0].set_value(weights)
+    rbm.params[1].set_value(np.zeros((3,), dtype='float32') - 0.5)
+    rbm.params[2].set_value(np.zeros((3,), dtype='float32') - 0.5)
+    X = T.tensor4()
+    upd, energy, grad = rbm.getCDUpdates(X, 1, steps=2)
+    gradUpdates = getMomentumUpdates(grad, rbm.params, alpha=0.1, momentum=0.0)
+    updates = upd + gradUpdates
+    learn = theano.function([X], energy, updates=updates, allow_input_downcast=True)
+    generate = theano.function([X], rbm.mean_vhv(X), updates=upd, allow_input_downcast=True)
+
+    for i in range(1000):
+        e = learn(img)
+        print("Energy: ", e)
+        generated = generate(img)[0].transpose(1, 2, 0) * 255
+        print(generated.max())
+        plt.imshow(generate(img)[0].transpose(1, 2, 0) * 255)
+        plt.pause(0.05)
+
 if __name__== '__main__':
-    ConvRBMTester()
+    med_ConvRBMTester()
